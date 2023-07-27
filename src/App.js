8d3fbbc4f96bac1e1ac6325b1a65920a29fd0788
berbolot <berbolotd@gmail.com>
2023-07-27 15:30:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Dropzone from 'react-dropzone';
 
-function App() {
+const YandexDiskUploader = () => {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const handleFileDrop = (acceptedFiles) => {
+    setSelectedFiles(acceptedFiles);
+  };
+
+  const handleFileUpload = () => {
+    const API_KEY = 'YOUR_YANDEX_DISK_API_KEY';
+    const UPLOAD_URL = 'https://cloud-api.yandex.net/v1/disk/resources/upload';
+
+    const uploadPromises = selectedFiles.map((file) => {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      return axios.post(UPLOAD_URL, formData, {
+        headers: {
+          Authorization: `OAuth ${API_KEY}`,
+        },
+      });
+    });
+
+    Promise.all(uploadPromises)
+      .then((responses) => {
+        console.log('Files uploaded successfully!');
+        console.log(responses);
+      })
+      .catch((error) => {
+        console.error('Error uploading files:', error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Dropzone onDrop={handleFileDrop} accept="image/*" multiple={true} maxSize={5242880}>
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drag and drop files here or click to select files (up to 100 images, max 5MB each).</p>
+            <ul>
+              {selectedFiles.map((file) => (
+                <li key={file.name}>{file.name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </Dropzone>
+      <button onClick={handleFileUpload} disabled={selectedFiles.length === 0}>
+        Upload Files
+      </button>
     </div>
   );
-}
+};
 
-export default App;
+export default YandexDiskUploader;
